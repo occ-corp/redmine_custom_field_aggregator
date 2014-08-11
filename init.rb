9a0747ac00c7091ed745f65c427c6412bc2832e0
Yusaku ONO <yono.05@gmail.com>
2014-08-11 13:55:13 +0900

    fix plugin's information

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'redmine'
 
-Redmine::Plugin.register :custom_field_aggregator do
-  name 'Redmine Time Entry Custom Field Aggregator plugin'
+Redmine::Plugin.register :redmine_custom_field_aggregator do
+  name 'Redmine Custom Field Aggregator plugin'
   author 'Yusaku ONO'
   description "This plugin enables to sum up spent time and custom field's values."
   version '0.0.1'
-  url 'http://git.es.occ.co.jp/redmine/custom_field_aggregator'
-  author_url 'http://git.es.occ.co.jp/redmine'
-  project_module :custom_field_aggregator do
+  url 'https://github.com/occ-corp/redmine_custom_field_aggregator'
+  author_url 'https://github.com/yono'
+  project_module :redmine_custom_field_aggregator do
     permission :view_table, {:aggregator => [:index]}, :public => true
     menu :project_menu, :view_table, { :controller => 'aggregator', :action => 'index'}, :caption => :cfa_menu, :param => :project_id
   end
